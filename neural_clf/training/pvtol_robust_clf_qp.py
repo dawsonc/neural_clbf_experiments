27f4bcf5da59b3ff602f0039bc05ecbb84212fb2
Charles Dawson <cbd@mit.edu>
2021-02-09 17:14:44 -0500

    increase LF convergence lambda

--- neural_clf/training/pvtol_robust_clf_qp.py
+++ neural_clf/training/pvtol_robust_clf_qp.py
@@ -80,7 +80,7 @@ scenarios = [
 
 # Define hyperparameters and define the learning rate and penalty schedule
 relaxation_penalty = 1.0
-clf_lambda = 0.0
+clf_lambda = 1.0
 safe_level = 5.0
 timestep = 0.001
 n_hidden = 48
