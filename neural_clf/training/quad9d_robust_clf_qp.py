df3cbe5c41858ba68193c71ecc6bac39cc1be5f7
Charles Dawson <cbd@mit.edu>
2021-02-22 10:43:52 -0500

    quad9d lr tweak

--- neural_clf/training/quad9d_robust_clf_qp.py
+++ neural_clf/training/quad9d_robust_clf_qp.py
@@ -123,7 +123,8 @@ init_controller_loss_coeff = 0.1
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = learning_rate * (0.9 ** (epoch // 3))
+    # lr = learning_rate * (0.9 ** (epoch // 3))
+    lr = learning_rate
     for param_group in optimizer.param_groups:
         param_group['lr'] = max(lr, 1e-4)
 
